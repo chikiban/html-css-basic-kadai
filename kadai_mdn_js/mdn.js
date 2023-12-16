f552416721ac0188b0af6aa350afd0a7c1cf31dd
chikiban <110600157+chikiban@users.noreply.github.com>
2023-12-16 21:46:10 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,8 +0,0 @@
-const date =  new Date();
-const [month, day ,year] = [
-    date.getMonth() + "月",
-    date.getDate() + "日",
-    date.getFullYear() + "年",
-];
-
-console.log(date.toLocaleDateString());
